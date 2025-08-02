928060867d82467c8055d64c8c30f358ae4dd632
James Tipping <jamestipping@protonmail.com>
2025-08-02 22:06:52 +0100

    add breadth first traversal

--- src/test/java/algorithms/graphs/BFS/GraphTest.java
+++ src/test/java/algorithms/graphs/BFS/GraphTest.java
@@ -32,4 +32,23 @@ public class GraphTest {
         g.BFS(0);
     }
 
+  @Test
+    public void DepthFirstSearch() {
+
+        g.addEdge(0, 1);
+        g.addEdge(1, 4);
+        g.addEdge(4, 6);
+        g.addEdge(6, 0);
+        g.addEdge(1, 5);
+        g.addEdge(5, 3);
+        g.addEdge(3, 0);
+        g.addEdge(5, 2);
+        g.addEdge(2, 7);
+
+        System.out.println("Following is Depth First Traversal "+
+                "(starting from vertex 0)");
+
+        g.DFS(0);
+    }
+
 }
